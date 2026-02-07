9b52d6bf4d11aa7ce185b273180b4424adc519eb
Biozidtonoy <150583477+Biozidtonoy@users.noreply.github.com>
2026-02-07 14:29:54 +0600

    added a form in main page

--- library.js
+++ library.js
@@ -14,8 +14,29 @@ function addBookToLibrary(bookTitle,bookAuthor ,totalPages) {
 }
 addBookToLibrary("science","tonoy",888);
 addBookToLibrary("commerce","tonni",200);
-addBookToLibrary("math","safwan",500)
+addBookToLibrary("math","safwan",500);
 console.log(myLibrary);
 
 
+function displayBooks(){
+    const container = document.querySelector(".container");
 
+    for(let i=0; i < myLibrary.length; i++){
+        const book = myLibrary[i];
+
+        const card = document.createElement("div");
+        card.classList.add("div-item");
+        container.appendChild(card);
+
+        card.innerHTML = `<h3>Title : ${book.title}</h3>
+        <p>Author : ${book.author}</p>
+        <p>Page: ${book.pages}</p>`
+    }
+}
+displayBooks();
+
+const btn1= document.querySelector(".btn1");
+btn1.addEventListener("click", ()=>{
+    
+    
+});
